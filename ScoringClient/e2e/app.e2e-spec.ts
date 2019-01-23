b1fd3ca46eda9151f2c319ea0943b6c3e84fcc33
bspjojo <thompsonjoseph0@gmail.com>
2019-01-23 20:12:26 +1300

    update ports and tests

--- ScoringClient/e2e/app.e2e-spec.ts
+++ ScoringClient/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('scoring-client App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toEqual('Welcome to app!');
+        expect(page.getParagraphText()).toEqual('Scoring app');
     });
 });
