1f6e2e97f16717edbd27d02a47ec29a2c3262872
bspjojo <thompsonjoseph0@gmail.com>
2019-01-31 20:40:52 +1300

    post result to api

--- ScoringClient/src/app/scoring/result-entry/result-entry.component.ts
+++ ScoringClient/src/app/scoring/result-entry/result-entry.component.ts
@@ -1,14 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+import { ScoringComponentService } from '../scoring-component.service';
 
 @Component({
     selector: 'score-result-entry',
     templateUrl: './result-entry.component.html',
     styleUrls: ['./result-entry.component.less']
 })
-export class ResultEntryComponent implements OnInit {
+export class ResultEntryComponent implements OnInit, OnDestroy {
+    public forms: { name: string, form: FormControl }[];
+    public validationGroup: FormGroup;
 
-    constructor() { }
+    private energyFormControl: FormControl;
+    private chocolateFormControl: FormControl;
+    private meatFormControl: FormControl;
+    private grainFormControl: FormControl;
+    private textilesFormControl: FormControl;
+
+    private ngUnsubscribe: Subject<void>;
+
+    constructor(private scoreComponentService: ScoringComponentService) {
+        this.ngUnsubscribe = new Subject();
+    }
 
     public ngOnInit(): void {
+        this.energyFormControl = new FormControl(null, [Validators.required, Validators.min(0)]);
+        this.chocolateFormControl = new FormControl(null, [Validators.required, Validators.min(0)]);
+        this.meatFormControl = new FormControl(null, [Validators.required, Validators.min(0)]);
+        this.grainFormControl = new FormControl(null, [Validators.required, Validators.min(0)]);
+        this.textilesFormControl = new FormControl(null, [Validators.required, Validators.min(0)]);
+
+        this.forms = [
+            {
+                name: 'Energy',
+                form: this.energyFormControl
+            },
+            {
+                name: 'Chocolate',
+                form: this.chocolateFormControl
+            },
+            {
+                name: 'Mean',
+                form: this.meatFormControl
+            },
+            {
+                name: 'Grain',
+                form: this.grainFormControl
+            },
+            {
+                name: 'Textiles',
+                form: this.textilesFormControl
+            }
+        ];
+
+        this.validationGroup = new FormGroup({});
+
+        for (let index = 0; index < this.forms.length; index++) {
+            const form = this.forms[index];
+
+            this.validationGroup.setControl(form.name, form.form);
+        }
+
+        console.log(this.validationGroup)
+    }
+
+    public ngOnDestroy(): void {
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
+    }
+
+    public async updateScores(): Promise<void> {
+        let requestObj = {
+            Energy: this.energyFormControl.value,
+            Chocolate: this.chocolateFormControl.value,
+            Meat: this.meatFormControl.value,
+            Grain: this.grainFormControl.value,
+            Textiles: this.textilesFormControl.value
+        };
+
+        let res = await this.scoreComponentService.updateScore(requestObj);
+        console.log(res);
     }
 }
--- ScoringClient/src/app/scoring/scoring.component.ts
+++ ScoringClient/src/app/scoring/scoring.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameSelectionService } from '../game-selection/game-selection.service';
 import { CountrySelection } from './country-selection/country-selection.model';
+import { ScoringComponentService } from './scoring-component.service';
 
 @Component({
     selector: 'score-scoring',
@@ -8,17 +9,25 @@ import { CountrySelection } from './country-selection/country-selection.model';
     styleUrls: ['./scoring.component.less']
 })
 export class ScoringComponent implements OnInit {
+    private selectedCountry: CountrySelection;
+    private apiResponse: any;
 
-    constructor(private gameSelectionService: GameSelectionService) { }
+    constructor(private gameSelectionService: GameSelectionService, private scoringComponentService: ScoringComponentService) { }
 
     public ngOnInit(): void {
     }
 
     public countrySelected(countrySelection: CountrySelection): void {
-        console.log(countrySelection);
+        this.selectedCountry = countrySelection;
+        this.scoringComponentService.country = countrySelection;
+        this.apiResponse = null;
     }
 
     public get show(): boolean {
         return this.gameSelectionService.game != null;
+    }
+
+    public get showResultEntry(): boolean {
+        return this.selectedCountry != null;
     }
 }
