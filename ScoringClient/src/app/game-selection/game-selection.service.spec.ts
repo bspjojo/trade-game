9ecf50897329fde993a8faee9b6288420d120a87
bspjojo <thompsonjoseph0@gmail.com>
2019-02-02 18:29:01 +1300

    GameSelectionService tests

--- ScoringClient/src/app/game-selection/game-selection.service.spec.ts
+++ ScoringClient/src/app/game-selection/game-selection.service.spec.ts
@@ -1,15 +1,58 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GameSelectionService } from './game-selection.service';
+import { ConfigService } from '../app-config/config.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 describe('GameSelectionService', () => {
+    let service: GameSelectionService;
+
     beforeEach(() => {
+        this.httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+        this.mockConfigService = {
+            config: {
+                apiUrl: 'api/url/'
+            }
+        };
+
+        this.httpClientSpyGetResponseContent = [{ data: 'value' }];
+        this.httpClientSpy.get.and.callFake(() => Observable.of(this.httpClientSpyGetResponseContent));
+
         TestBed.configureTestingModule({
-            providers: [GameSelectionService]
+            providers: [
+                GameSelectionService,
+                { provide: ConfigService, useValue: this.mockConfigService },
+                { provide: HttpClient, useValue: this.httpClientSpy }
+            ]
+        });
+
+        service = TestBed.get(GameSelectionService);
+    });
+
+    describe('game', () => {
+        it('should be able to get and set a value', () => {
+            let val = { id: 'id', name: 'name' } as any;
+            service.game = val;
+
+            expect(service.game).toBe(val);
         });
     });
 
-    it('should be created', inject([GameSelectionService], (service: GameSelectionService) => {
-        expect(service).toBeTruthy();
-    }));
+    describe('getGames', () => {
+        it('should call to the configured api url /api/game/games', () => {
+            service.getGames();
+
+            expect(this.httpClientSpy.get).toHaveBeenCalledWith('api/url/api/game/games');
+        });
+
+        it('should return a promise that resolves to the result', async (done) => {
+            let p = await service.getGames();
+
+            expect(p).toBe(this.httpClientSpyGetResponseContent);
+
+            done();
+        });
+    });
 });
--- ScoringClient/src/polyfills.ts
+++ ScoringClient/src/polyfills.ts
@@ -65,3 +65,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/observable/of';
