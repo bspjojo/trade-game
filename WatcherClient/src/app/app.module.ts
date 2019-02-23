939737656c420003d933de64d1455350fa9f43fc
bspjojo <thompsonjoseph0@gmail.com>
2019-02-23 18:08:30 +1300

    first component

--- WatcherClient/src/app/app.module.ts
+++ WatcherClient/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JoinGameComponent } from './pages/join-game/join-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JoinGameComponent
   ],
   imports: [
     BrowserModule,
---
+++ WatcherClient/src/app/pages/join-game/join-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'watcher-join-game',
+  templateUrl: './join-game.component.html',
+  styleUrls: ['./join-game.component.less']
+})
+export class JoinGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
