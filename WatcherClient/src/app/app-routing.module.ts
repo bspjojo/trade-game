fa4848d0583e5e39d25afba165e39e39d764051c
bspjojo <thompsonjoseph0@gmail.com>
2019-04-13 17:44:50 +1200

    Detailed view of game scores (#29)
    * lint fix
    
    * detailed view

--- WatcherClient/src/app/app-routing.module.ts
+++ WatcherClient/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JoinGameComponent } from './pages/join-game/join-game.component';
 import { SummaryViewComponent } from './pages/summary-view/summary-view.component';
+import { DetailedViewComponent } from './pages/detailed-view/detailed-view.component';
 
 const routes: Routes = [{
     path: '', component: JoinGameComponent
 }, {
     path: 'scores', children: [{
         path: 'summary', component: SummaryViewComponent
+    }, {
+        path: 'detailed', component: DetailedViewComponent
     }]
 }];
 
