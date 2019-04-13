bba153ef3f94d06d0ab8ad64215f3bf03dff5588
bspjojo <thompsonjoseph0@gmail.com>
2019-04-13 16:35:16 +1200

    lint fix

--- WatcherClient/src/app/app.component.ts
+++ WatcherClient/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'watcher-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-    title = 'WatcherClient';
+export class AppComponent implements OnInit {
+    public title: string;
+
+    public ngOnInit(): void {
+        this.title = 'WatcherClient';
+    }
 }
