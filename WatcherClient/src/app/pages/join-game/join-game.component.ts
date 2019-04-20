62e19aeb219771b85e3842d300bac25f5c8535f0
bspjojo <thompsonjoseph0@gmail.com>
2019-04-20 19:29:00 +1200

    observe game current year changes

--- WatcherClient/src/app/pages/join-game/join-game.component.ts
+++ WatcherClient/src/app/pages/join-game/join-game.component.ts
@@ -3,7 +3,9 @@ import { FormControl } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { GameApiService } from 'src/app/game-services/game-api.service';
+import { GameDataService } from 'src/app/game-services/game-data.service';
 import { GameHubService } from 'src/app/game-services/game-hub.service';
+import { Game } from 'src/app/game-services/game.model';
 
 import { GameSelection } from './game-selection.model';
 import { GameSelectionService } from './game-selection.service';
@@ -16,35 +18,48 @@ import { GameSelectionService } from './game-selection.service';
 export class JoinGameComponent implements OnInit, OnDestroy {
     public games: GameSelection[];
     public gameSelectionControl: FormControl;
-    public selectedGame: GameSelection;
+    public gameYearControl: FormControl;
+    private gameChangeSubject: Subject<void>;
+    public game: Game;
 
     private ngUnsubscribe: Subject<void>;
 
     constructor(private gameSelectionService: GameSelectionService, private gameService: GameHubService,
-        private gameApiService: GameApiService) {
+        private gameDataService: GameDataService, private gameApiService: GameApiService) {
         this.ngUnsubscribe = new Subject();
     }
 
     public async ngOnInit(): Promise<void> {
+        this.gameChangeSubject = new Subject();
         let game = this.gameSelectionService.game;
-        this.selectedGame = game;
 
         let gameId = game != null ? game.id : null;
         this.gameSelectionControl = new FormControl(gameId);
-        this.gameSelectionControl.valueChanges.pipe(takeUntil(this.ngUnsubscribe)).subscribe((selectedGame: string) => {
+        this.gameSelectionControl.valueChanges.pipe(takeUntil(this.ngUnsubscribe)).subscribe((selectedGameId: string) => {
             let pGame = this.gameSelectionService.game;
-            this.selectedGame = this.games.find(v => v.id === selectedGame);
-            this.gameSelectionService.game = this.selectedGame;
+            let selectedGame = this.games.find(v => v.id === selectedGameId);
+            this.gameSelectionService.game = selectedGame;
 
             if (pGame != null) {
                 this.gameService.leaveGame(pGame.id);
             }
 
-            if (selectedGame != null) {
-                this.gameService.joinGame(selectedGame);
+            if (selectedGameId != null) {
+                this.gameService.joinGame(selectedGameId);
             }
 
-            this.gameApiService.setGameScores(selectedGame);
+            this.gameApiService.setGameScores(selectedGameId);
+        });
+
+        this.gameDataService.gameSubject.pipe(takeUntil(this.ngUnsubscribe)).subscribe((gameData: Game) => {
+            this.game = gameData;
+            this.gameYearControl = new FormControl(this.game != null ? this.game.currentYear : null);
+
+            this.gameChangeSubject.next();
+
+            this.gameYearControl.valueChanges.pipe(takeUntil(this.gameChangeSubject)).subscribe((year: number) => {
+                console.log(year);
+            });
         });
 
         this.games = [];
--- WatcherClient/src/app/pages/join-game/join-game.module.ts
+++ WatcherClient/src/app/pages/join-game/join-game.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material';
 import { GameServicesModule } from 'src/app/game-services/game-services.module';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 import { AppConfigModule } from '../../app-config/app-config.module';
 import { GameSelectionService } from './game-selection.service';
@@ -17,7 +18,8 @@ import { JoinGameComponent } from './join-game.component';
         HttpClientModule,
         AppConfigModule,
         GameServicesModule,
-        MatSelectModule
+        MatSelectModule,
+        PipesModule
     ],
     declarations: [JoinGameComponent],
     exports: [JoinGameComponent],
